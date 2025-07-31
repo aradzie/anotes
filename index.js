8676f880906d4fbe24b0df050a0ae0b8ee118a35
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2025-07-31 14:51:38 +0200

    apply note templates

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 import { marked } from "marked";
 import { globSync, readFileSync, writeFileSync } from "node:fs";
+import { formatNotes, parseNotes } from "./lib/anki.js";
 import { latexOptions } from "./lib/latex.js";
 
 marked.use(latexOptions);
@@ -17,91 +18,12 @@ function main(deckFile) {
   for (const inputFile of globSync("**/*.txt", {
     exclude: [deckFile, "tmp"],
   })) {
-    parseFile(inputFile, notes);
+    console.log(`Parsing file "${inputFile}"`);
+    const text = readFileSync(inputFile, "utf-8");
+    parseNotes(text, notes);
   }
   console.log(`Parsed ${notes.length} note(s)`);
   console.log(`Writing file "${deckFile}"`);
   writeFileSync(deckFile, formatNotes(notes));
   writeFileSync("_notes.json", JSON.stringify(notes, null, 2));
-}
-
-function parseFile(inputFile, notes) {
-  console.log(`Parsing file "${inputFile}"`);
-  const text = readFileSync(inputFile, "utf-8");
-
-  let type = "Basic";
-  let deck = "";
-  let tags = "";
-
-  let state = "front";
-
-  let front = "";
-  let back = "";
-
-  for (let line of text.split(/\n/g)) {
-    if (line.startsWith("#")) {
-      continue;
-    }
-    if (line.startsWith("!type:")) {
-      type = line.substring(6).trim();
-      continue;
-    }
-    if (line.startsWith("!deck:")) {
-      deck = line.substring(6).trim();
-      continue;
-    }
-    if (line.startsWith("!tags:")) {
-      tags = line.substring(6).trim();
-      continue;
-    }
-    if (line.startsWith("---")) {
-      state = "back";
-      continue;
-    }
-    if (line.startsWith("===")) {
-      front = marked.parse(front.trim()).trim();
-      back = marked.parse(back.trim()).trim();
-      notes.push({type, deck, tags, front, back});
-      state = "front";
-      front = "";
-      back = "";
-      continue;
-    }
-    switch (state) {
-      case "front":
-        if (front) {
-          front += "\n";
-        }
-        front += line;
-        break;
-      case "back":
-        if (back) {
-          back += "\n";
-        }
-        back += line;
-        break;
-    }
-  }
-}
-
-function formatNotes(notes) {
-  const lines = [];
-  lines.push(`#separator:semicolon`);
-  lines.push(`#html:true`);
-  lines.push(`#notetype column:1`);
-  lines.push(`#deck column:2`);
-  lines.push(`#tags column:3`);
-  for (const {type, deck, tags, front, back} of notes) {
-    lines.push([type, deck, tags, front, back].map(formatField).join(";"));
-  }
-  lines.push("");
-  return lines.join("\n");
-}
-
-function formatField(value) {
-  if (value.includes(";") || value.includes("\n") || value.includes('"')) {
-    return `"${value.replaceAll('"', '""')}"`;
-  } else {
-    return value;
-  }
 }
