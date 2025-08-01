66c7b0682666c95f887de3fdda246a6861e2c166
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2025-08-01 22:14:53 +0200

    export unique note ids

--- build.js
+++ build.js
@@ -5,21 +5,19 @@
  */
 
 import { readFileSync, writeFileSync } from "node:fs";
-import { join } from "node:path";
 import { formatNotes, formatNotesJson, parseNotes } from "./lib/anki.js";
-import { findNoteFiles } from "./lib/io.js";
+import { findNoteFiles, pathTo } from "./lib/io.js";
 
 main();
 
 function main() {
   const notes = [];
-  const cwd = process.cwd();
   for (const file of findNoteFiles()) {
     console.log(`Parsing file "${file}"`);
     const text = readFileSync(file, "utf-8");
     parseNotes(file, text, notes);
   }
   console.log(`Parsed ${notes.length} note(s)`);
-  writeFileSync(join(cwd, "_notes.txt"), formatNotes(notes));
-  writeFileSync(join(cwd, "_notes.json"), JSON.stringify(formatNotesJson(notes), null, 2));
+  writeFileSync(pathTo("_notes.txt"), formatNotes(notes));
+  writeFileSync(pathTo("_notes.json"), JSON.stringify(formatNotesJson(notes), null, 2));
 }
